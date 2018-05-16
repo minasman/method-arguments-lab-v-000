31ae8331d9d5f2e73d8468d758912b6f8f3c74af
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-16 20:44:58 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction(name, language)
+def introduction(name)
   puts "Hi, my name is #{name}.\n"
+end
+
+def introduction-with-language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program #{language}.\n"
 end
